3be2832e2ab92a261410b62d1c36ebb9e59795fb
yasosaran01 <30857815+yasosaran01@users.noreply.github.com>
2020-12-29 15:46:28 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+let words =[ {
+    question:"brave",
+    options:["courage","fear","sad"],
+    answer:"courage"
+},
+    {
+    question:"endure",
+    options:["persist","flaw","strange"],
+    answer:"persist"
+},
+{
+    question:"harass",
+    options:["reason","observant","torment"],
+    answer:"torment"
+},
+{
+    question:"baffle",
+    options:["confuse","clear","transparent"],
+    answer:"confuse"
+},
+]
